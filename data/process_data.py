d64e3c0770514a05cd02ff3f863966de79fdb6d0
Jonas Kirchner <kirchjna@schaeffler.com>
2021-03-28 09:48:31 +0200

    ETL pipeline

---
+++ data/process_data.py
@@ -0,0 +1,67 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on = 'id') 
+    return df
+
+
+def clean_data(df):
+    categories = df['categories'].str.split(";",expand=True)
+    row = categories.loc[0]
+    category_colnames =row.str[:-2]
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    df = df.drop(['categories'], axis =1)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(keep='last', inplace = True)
+    
+    return df
+
+
+def save_data(df, database_filename):
+    CONNECTION_STRING = f"sqlite:///{database_filename}"
+    engine = create_engine(CONNECTION_STRING)
+    if not engine.dialect.has_table(engine,'disaster_data'):
+        df.to_sql('disaster_data', engine, index=False) 
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
